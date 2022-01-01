1ab326f68358b5533a2996f75e9894d6ad9ff52e
Hoàn Ngọc Lê <hoanngocle.93@gmail.com>
2022-01-01 20:27:41 +0700

    Add Sanctum authentication

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller as BaseController;
+use App\Models\User;
+
+class AuthController extends BaseController
+{
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function test(Request $request): string
+    {
+        return "123";
+    }
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $auth = Auth::user();
+            $success['token'] = $auth->createToken('LaravelSanctumAuth')->plainTextToken;
+            $success['name'] = $auth->name;
+
+            return $this->handleResponse($success, 'User logged-in!');
+        } else {
+            return $this->handleError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->handleError($validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('LaravelSanctumAuth')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return $this->handleResponse($success, 'User successfully registered!');
+    }
+}
