334175c4459c63e5d6337e43eee35bbf665df7a9
Nyx Darkness <hoanngocle.93@gmail.com>
2022-04-22 13:27:06 +0700

    Update user seeder

--- database/migrations/2022_03_03_131644_create_heroes_table.php
+++ database/migrations/2022_03_03_131644_create_heroes_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create(config('constant.TBL.HERO'), function (Blueprint $table) {
             $table->id();
-            $table->string('code');
             $table->string('name')->unique();
             $table->text('bio');
             $table->string('avatar');
