27fed836705590cfed707eff2d7bed22e4475b9e
Nyx Darkness <hoanngocle.93@gmail.com>
2022-04-28 09:35:23 +0700

    Make Hero Seeder

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Imports\HeroImport;
 use App\Models\Hero;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Maatwebsite\Excel\Facades\Excel;
 
