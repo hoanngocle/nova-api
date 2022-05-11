1632f5c9d5b9ad8f205854860e015970d4249aca
Nyx Darkness <hoanngocle.93@gmail.com>
2022-05-11 17:28:03 +0700

    Add hero image

---
+++ app/Models/HeroImage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class HeroImage extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function hero(): BelongsTo
+    {
+        return $this->belongsTo(Hero::class);
+    }
+}
